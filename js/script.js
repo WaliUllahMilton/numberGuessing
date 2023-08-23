2b59e66d9daf2142f924d2c965e2782b230ec619
waliUllahMilton <waliullahmiltoncse@gmail.com>
2023-08-23 20:25:34 +0600

    final

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 let playerOneName = document.querySelector(".pl-one");
 let buttonOneName = document.querySelector(".pl-one-name");
 let buttonOneData = document.querySelector(".pl-one-data");
-let playerOne = document.querySelector(".name-one")
-let playerTwo = document.querySelector(".name-two")
-let playerOneNumber = document.querySelector(".input-one")
+let playerOne = document.querySelector(".name-one");
+let playerTwo = document.querySelector(".name-two");
+let playerOneNumber = document.querySelector(".input-one");
+let playerTwoNumber = document.querySelector(".input-two");
 let buttonTwoName = document.querySelector(".pl-two-name");
 let buttonTwoData = document.querySelector(".pl-two-data");
 let activePlayerOne = document.querySelector(".pl-one-display");
@@ -12,26 +13,33 @@ let playerTwoName = document.querySelector(".pl-two");
 let key = document.querySelector(".secret-key");
 let guessKey = document.querySelector(".guess-key");
 let error = document.querySelector("p");
+let guessError = document.querySelector(".gs-error");
 let result = document.querySelector(".result");
+let winner = document.querySelector(".winner");
+let count = document.querySelector(".chances");
 
 buttonOneName.addEventListener("click", function() {
-    let name = playerOneName.value;
+    playerOneName = playerOneName.value;
     displayNone(playerOne);
-    activePlayerOne.innerHTML = name;
+    activePlayerOne.innerHTML = playerOneName;
+    playerOneNumber.style.display = "inline-flex";
     buttonOneData.addEventListener("click", function() {
         let secretKey = key.value;
         if (secretKey <= 10 && secretKey > 0) {
             displayNone(playerOneNumber);
             buttonTwoName.addEventListener("click", () => {
-                let name = playerTwoName.value;
+                playerTwoName = playerTwoName.value;
                 displayNone(playerTwo);
-                activePlayerTwo.innerHTML = name;
+                activePlayerTwo.innerHTML = playerTwoName;
+                playerTwoNumber.style.display = "inline-flex"
                 buttonTwoData.addEventListener("click", () => {
                     let guessNumber = guessKey.value;
                     if (secretKey == guessNumber) {
-                        result.innerHTML = "Player two won";
+                        result.innerHTML = playerTwoName + " won";
+                        playerTwoNumber.style.display = "none"
                     } else {
-                        result.innerHTML = "player one won";
+                        result.innerHTML = playerOneName + " won";
+                        playerTwoNumber.style.display = "none"
                     }
                 })
 
